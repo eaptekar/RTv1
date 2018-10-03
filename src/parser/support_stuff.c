ef990127331ca6f56917dac57913ac67702da966
vovche <menkar.fmail@gmail.com>
2018-10-04 02:43:32 +0300

    normal find_value. now can handle bad input

--- src/parser/support_stuff.c
+++ src/parser/support_stuff.c
@@ -5,8 +5,6 @@
 
 void _getline(char* buff, char* cursor)
 {
-    while (ft_isspase(*cursor))
-        ++cursor;
     while (*cursor && *cursor != '\n')
         *buff++ = *cursor++;
     *buff = '\0';
@@ -16,15 +14,22 @@ int find_value(char* cursor, char* buff, char* value)
 {
     char line[LINE_BUFF_SIZE];
 
-    while (*cursor)
+    while (*cursor != '}' && *cursor)
     {
+        while (ft_isspase(*cursor))
+            ++cursor;
         _getline(line, cursor);
-        if (ft_strncpy(line, value, ft_strlen(value)))
+        if (!ft_strncmp(line, value, ft_strlen(value)))
         {
             _getline(buff, line + ft_strlen(value)); 
             break;
         }
         cursor += ft_strlen(line);
+    }
+    if (!(*cursor) || *cursor == '}')
+    {
+        ft_putendl(value);
+        ERROR(": not found");
     }
     return 0;
 }
