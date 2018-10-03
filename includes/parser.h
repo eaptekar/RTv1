38ce3712e46d1a572bb422d8985001eae40a1315
vovche <menkar.fmail@gmail.com>
2018-10-03 22:29:41 +0300

    added cursor

--- includes/parser.h
+++ includes/parser.h
@@ -8,7 +8,7 @@
 t_scene* parse_file(char* filename);
 
 //returns pointer to function to read item; 
-char* (*get_reader(char* item_name))(t_scene* scene, char* cursor);
+char* (*get_reader(char** item_name))(t_scene* scene, char* cursor);
 
 //readers
 char* reader_scene(t_scene* scene, char* cursor);
--- src/parser.c
+++ src/parser.c
@@ -12,27 +12,28 @@ t_scene* parse_file(char* filename)
 {
     char file[FILE_BUFF_SIZE];
     int fd;
-    char* (*reader)(t_scene* scene, char* cursor);
+    char* cursor;
 
     fd = open(filename, 'r');
     read(fd, file, FILE_BUFF_SIZE - 2);
     file[FILE_BUFF_SIZE - 1] = '\0';   
-    
-    printf("%d\n", !ft_strncmp("1234", "1235", 3));
+    cursor = file; 
     t_scene *scene = (t_scene*)malloc(sizeof(t_scene));
-    reader = get_reader(file);
-    reader(scene, "abc");
+    get_reader(&cursor)(scene, cursor);
     printf("%d", scene->recursion_depth);
     close(fd);
     return scene;
 }
 
-char* (*get_reader(char* item_name))(t_scene*, char* cursor)
+char* (*get_reader(char** item_name))(t_scene*, char* cursor)
 {
-    while (isspase(*item_name))
-        ++item_name;
-    if (!ft_strncmp(item_name, "scene", 5))
+    while (isspase(**item_name))
+        ++(*item_name);
+    if (!ft_strncmp(*item_name, "scene", 5))
+    {
+        *item_name += 5;
         return &reader_scene;
+    }
     return NULL;
 }
 
@@ -40,6 +41,7 @@ char *reader_scene(t_scene* scene, char* cursor)
 {
     while (isspase(*cursor))
         ++cursor;
+    printf("%c\n", *cursor);
     scene->recursion_depth = 3;
     return NULL;
 }
